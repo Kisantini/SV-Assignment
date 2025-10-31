69110b5b68f148208af4fcb274cdd3f2243933df
Kisantini <s22a0069@siswa.umk.edu.my>
2025-11-01 03:10:46 +0800

    Remove icons from page definitions in app.py

--- app.py
+++ app.py
@@ -6,9 +6,9 @@ st.set_page_config(
 
 home = st.Page('Home.py', title= 'Introduction', icon=":material/home:")
 
-obj1 = st.Page('Objective1.py' , title = "Distribution of Rape Cases Across States ",default=True, icon=":material/1:")
-obj2 = st.Page('Objective2.py' , title = "Trends and Annual Changes ",default=True, icon=":material/2:")
-obj3 = st.Page('Objective3.py' , title = "Correlation Between Rape Cases and Related Crimes ",default=True, icon=":material/3:")
+obj1 = st.Page('Objective1.py' , title = "Distribution of Rape Cases Across States ",default=True,)
+obj2 = st.Page('Objective2.py' , title = "Trends and Annual Changes ",default=True)
+obj3 = st.Page('Objective3.py' , title = "Correlation Between Rape Cases and Related Crimes ",default=True)
 
 pg = st.navigation(
         {
